edc50373f80b79c8660778bd48fc4d08887e58c4
nmskm240 <utc.am240@gmail.com>
2025-11-21 02:27:07 +0000

    dev 高速化のためinfluxdbからparquet+duckdbに移行

--- blueOcean/ohlcv.py
+++ blueOcean/ohlcv.py
@@ -2,17 +2,18 @@ from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from datetime import UTC, datetime
+from enum import IntEnum
 import logging
+from pathlib import Path
 import time
 from typing import Generator
+import duckdb
 import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 from dataclasses import asdict
 import ccxt
-from influxdb_client import Point
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
 
-BUCKET_NAME = "historical_data"
-MEASUREMENT_NAME = "ohlcvs"
 
 logger = logging.getLogger(__name__)
 
@@ -20,50 +21,11 @@ logger = logging.getLogger(__name__)
 @dataclass
 class Ohlcv:
     time: datetime
-    symbol: str
-    source: str
     open: float
     high: float
     low: float
     close: float
     volume: float
-    tick_volume: int | None = None
-    spread: int | None = None
-
-    def to_point(self) -> Point:
-        point = (
-            Point(MEASUREMENT_NAME)
-            .tag("symbol", self.symbol)
-            .tag("source", self.source)
-            .field("open", self.open)
-            .field("high", self.high)
-            .field("low", self.low)
-            .field("close", self.close)
-            .field("volume", self.volume)
-            .time(self.time)
-        )
-
-        if self.tick_volume is not None:
-            point.field("tick_volume", self.tick_volume)
-        if self.spread is not None:
-            point.field("spread", self.spread)
-
-        return point
-
-    @classmethod
-    def from_point(cls, point: Point):
-        return cls(
-            time=point.get_time(),
-            symbol=point.get_tag("symbol"),
-            source=point.get_tag("source"),
-            open=point.get_field("open"),
-            high=point.get_field("high"),
-            low=point.get_field("low"),
-            close=point.get_field("close"),
-            volume=point.get_field("volume"),
-            tick_volume=point.get_field("tick_volume"),
-            spread=point.get_field("spread"),
-        )
 
     @classmethod
     def to_dataframe(cls, ohlcvs: list[Ohlcv]) -> pd.DataFrame:
@@ -75,39 +37,27 @@ class Ohlcv:
     @classmethod
     def from_dataframe(cls, df: pd.DataFrame) -> list[Ohlcv]:
         return [cls(**row) for row in df.reset_index(drop=True).to_dict("records")]
+    
 
-    @classmethod
-    def from_flux_record(cls, record, symbol: str, source: str):
-        values = record.values
+class Timeframe(IntEnum):
+    ONE_MINUTE = 1
+    FIVE_MINUTE = 5
+    FIFTEEN_MINUTE = 15
+    ONE_HOUR = 60
+    FOUR_HOUR = 240
+    ONE_DAY = 1440
 
-        # Ensure essential fields are present
-        required_fields = ["open", "high", "low", "close", "volume"]
-        if not all(
-            field in values and values[field] is not None for field in required_fields
-        ):
-            raise ValueError()
-
-        return cls(
-            time=values["_time"],
-            symbol=symbol,
-            source=source,
-            open=values["open"],
-            high=values["high"],
-            low=values["low"],
-            close=values["close"],
-            volume=values["volume"],
-            tick_volume=values.get("tick_volume"),
-            spread=values.get("spread"),
-        )
+    def to_duck(self) -> str:
+        return f"'{int(self)} minutes'"
 
 
 class IOhlcvRepository(metaclass=ABCMeta):
     @abstractmethod
-    def save(self, ohlcv: list[Ohlcv]):
+    def save(self, ohlcv: list[Ohlcv], source: str, symbol: str):
         raise NotImplementedError()
 
     @abstractmethod
-    def get_latest_timestamp(self, exchange: str, symbol: str) -> datetime | None:
+    def get_latest_timestamp(self, source: str, symbol: str) -> datetime | None:
         raise NotImplementedError()
 
     @abstractmethod
@@ -115,111 +65,87 @@ class IOhlcvRepository(metaclass=ABCMeta):
         self,
         symbol: str,
         source: str,
+        interval: Timeframe = Timeframe.ONE_MINUTE,
         start_date: datetime | None = None,
         end_date: datetime | None = None
     ) -> list[Ohlcv]:
         raise NotImplementedError()
 
-
 class OhlcvRepository(IOhlcvRepository):
-    def __init__(self, client: InfluxDBClientAsync):
-        self.client = client
-        self.create_bucket_if_not_exists()
+    def __init__(self, base_path: str = "./data"):
+        self._base_dir = base_path
+        self.__con = duckdb.connect()
 
-    async def save(self, ohlcvs: list[Ohlcv]):
-        """OHLCVデータのリストをバッチで書き込みます。"""
-        points = [o.to_point() for o in ohlcvs]
-        write_api = self.client.write_api()
-        await write_api.write(bucket=BUCKET_NAME, org=self.client.org, record=points)
-        logger.info(f"Wrote {len(ohlcvs)} ohlcvs to InfluxDB.")
+    def _parse_from_symbol_to_dir(self, symbol: str) -> str:
+        return symbol.replace("/", "_")
 
-    async def create_bucket_if_not_exists(self):
-        buckets_api = self.client.buckets_api()
-        bucket = await buckets_api.find_bucket_by_name(BUCKET_NAME)
-        if not bucket:
-            logger.info(f"Create bucket {BUCKET_NAME}")
-            await buckets_api.create_bucket(bucket_name=BUCKET_NAME)
+    def save(self, ohlcv, source, symbol):
+        df = Ohlcv.to_dataframe(ohlcv)
+        df = df.sort_index()
 
-    async def get_latest_timestamp(self, exchange: str, symbol: str) -> datetime | None:
-        """InfluxDBから指定されたシンボルと時間足の最新のタイムスタンプを取得します。"""
-        query = f"""
-        from(bucket: "{BUCKET_NAME}")
-          |> range(start: 0)
-          |> filter(fn: (r) => r._measurement == "{MEASUREMENT_NAME}")
-          |> filter(fn: (r) => r.source == "{exchange}")
-          |> filter(fn: (r) => r.symbol == "{symbol}")
-          |> last()
+        df["year_month"] = df.index.strftime("%Y-%m")
+
+        for ym, chunk in df.groupby("year_month"):
+            symbol_dir = self._parse_from_symbol_to_dir(symbol)
+            out_dir = Path(self._base_dir, source, symbol_dir)
+            out_dir.mkdir(parents=True, exist_ok=True)
+            filename = Path(out_dir, f"{ym}.parquet")
+            chunk = chunk.drop(columns=["year_month"])
+            chunk["time"] = chunk.index
+            chunk = chunk[["time", "open", "high", "low", "close", "volume"]]
+            table = pa.Table.from_pandas(chunk,preserve_index=False)
+
+            if filename.exists():
+                old = pq.read_table(filename).to_pandas()
+                merged = ( 
+                    pd.concat([old, chunk], ignore_index=True) 
+                    .drop_duplicates(subset=["time"]) 
+                    .sort_values("time") )
+                table = pa.Table.from_pandas(merged, preserve_index=False)
+
+            pq.write_table(table, filename)
+
+    def get_latest_timestamp(self, source, symbol):
+        symbol_dir = self._parse_from_symbol_to_dir(symbol)
+        path = Path(self._base_dir, source, symbol_dir, "*.parquet")
+
+        sql = f"""
+            SELECT max(time) AS latest
+            FROM read_parquet(`{path}`)
         """
-        api = self.client.query_api()
-        tables = await api.query(query, org=self.client.org)
-        if not tables or not tables[0].records:
+
+        try:
+            row = self.__con.execute(sql).fetchone()
+            return pd.to_datetime(row[0]).to_pydatetime()
+        except:
             return None
-        return tables[0].records[0].get_time()
+        
+    def find(self, symbol, source, timeframe = Timeframe.ONE_MINUTE, start_date = None, end_date = None):
+        symbol_dir = self._parse_from_symbol_to_dir(symbol)
+        path = Path(self._base_dir, source, symbol_dir, "*.parquet")
 
-    async def find(
-        self,
-        symbol: str,
-        source: str,
-        start_date: datetime | None = None,
-        end_date: datetime | None = None,
-        timeframe: str | None = None,
-    ) -> list[Ohlcv]:
-        """指定された条件でOhlcvデータ検索"""
-        start_date = start_date or datetime(1970, 1, 1, tzinfo=UTC)
-        end_date = end_date or datetime.now(UTC)
+        where = []
+        if start_date:
+            where.append(f"time >= '{start_date.isoformat()}'")
+        if end_date:
+            where.append(f"time <= '{end_date.isoformat()}'")
 
-        ohlcvs: list[Ohlcv] = []
-        api = self.client.query_api()
-        start_dates = pd.date_range(start_date, end_date, freq="MS")
-
-        for start in start_dates:
-            end = (start + pd.offsets.MonthEnd()).to_pydatetime()
-            query = self._build_query(symbol, source, start, end, timeframe)
-            tables = await api.query(query, org=self.client.org)
-            for table in tables:
-                for record in table.records:
-                    ohlcv = Ohlcv.from_flux_record(record, symbol, source)
-                    if ohlcv:
-                        ohlcvs.append(ohlcv)
-
-        ohlcvs.sort(key=lambda x: x.time)
-
-        return ohlcvs
-
-    def _build_query(
-        self,
-        symbol: str,
-        source: str,
-        start: datetime,
-        end: datetime,
-        timeframe,
-    ) -> str:
-        base_query = f"""
-        from(bucket: "{BUCKET_NAME}")
-            |> range(start: {start.isoformat()}, stop: {end.isoformat()})
-            |> filter(fn: (r) => r._measurement == "{MEASUREMENT_NAME}")
-            |> filter(fn: (r) => r.symbol == "{symbol}")
-            |> filter(fn: (r) => r.source == "{source}")
-        """
-
-        if not timeframe or timeframe == "1m":
-            return f"""{base_query}
-                |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+        where_sql = "WHERE " + " AND ".join(where) if where else ""
+        sql = f"""
+                SELECT
+                    time_bucket(INTERVAL {timeframe.to_duck()}, time) AS time,
+                    first(open) AS open,
+                    max(high) AS high,
+                    min(low) AS low,
+                    last(close) AS close,
+                    sum(volume) AS volume
+                FROM read_parquet('{path}')
+                {where_sql}
+                GROUP BY 1
+                ORDER BY time
             """
-        else:
-            return f"""
-            base = {base_query}
-
-            o = base |> filter(fn: (r) => r._field == "open") |> aggregateWindow(every: {timeframe}, fn: first, createEmpty: false)
-            h = base |> filter(fn: (r) => r._field == "high") |> aggregateWindow(every: {timeframe}, fn: max, createEmpty: false)
-            l = base |> filter(fn: (r) => r._field == "low") |> aggregateWindow(every: {timeframe}, fn: min, createEmpty: false)
-            c = base |> filter(fn: (r) => r._field == "close") |> aggregateWindow(every: {timeframe}, fn: last, createEmpty: false)
-            v = base |> filter(fn: (r) => r._field == "volume") |> aggregateWindow(every: {timeframe}, fn: sum, createEmpty: false)
-            tv = base |> filter(fn: (r) => r._field == "tick_volume") |> aggregateWindow(every: {timeframe}, fn: sum, createEmpty: false)
-
-            union(tables: [o, h, l, c, v, tv])
-                |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
-            """
+        df = self.__con.execute(sql).df()
+        return Ohlcv.from_dataframe(df)
 
 
 class OhlcvFetcher(metaclass=ABCMeta):
@@ -265,8 +191,6 @@ class CcxtOhlcvFetcher(OhlcvFetcher):
                 columns=["time", "open", "high", "low", "close", "volume"],
             )
             df["time"] = pd.to_datetime(df["time"], unit="ms", utc=True)
-            df["symbol"] = symbol
-            df["source"] = exchange.name
 
             if not df.empty:
                 logger.info(
