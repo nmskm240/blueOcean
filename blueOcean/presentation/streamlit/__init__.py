00435490b1dcc44643dda98318166235010ee1c5
nmskm240 <utc.am240@gmail.com>
2025-12-14 11:21:31 +0000

    feat: Streamlit上でレポートメタ情報とHTMLを表示

--- blueOcean/presentation/streamlit/__init__.py
+++ blueOcean/presentation/streamlit/__init__.py
@@ -7,6 +7,7 @@ from blueOcean.presentation.streamlit.contents.strategies import strategy_page
 
 def setup():
     st.set_page_config(page_title="BlueOcean", layout="wide")
+    st.context.theme
 
     nav = st.navigation(
         {
@@ -21,10 +22,15 @@ def setup():
                     title="Playground",
                 ),
                 st.Page(
+                    "./blueOcean/presentation/streamlit/contents/reports.py",
+                    title="Reports",
+                ),
+                st.Page(
                     "./blueOcean/presentation/streamlit/contents/bots.py", title="Bots"
                 ),
                 st.Page(
-                    "./blueOcean/presentation/streamlit/contents/accounts.py", title="Accounts"
+                    "./blueOcean/presentation/streamlit/contents/accounts.py",
+                    title="Accounts",
                 ),
             ],
             "Strategies": [
