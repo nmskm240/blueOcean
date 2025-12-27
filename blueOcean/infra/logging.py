480167ce12d807b0b2e94b90da9140675c7e887f
nmskm240 <utc.am240@gmail.com>
2025-12-27 12:45:28 +0000

    fix ログをファイルローテンションするように

--- blueOcean/infra/logging.py
+++ blueOcean/infra/logging.py
@@ -1,19 +1,6 @@
 import logging
 import sys
-from datetime import datetime
-from queue import Queue
-
-log_queue = Queue()
-
-
-class StreamlitHandler(logging.Handler):
-    def __init__(self, queue: Queue = log_queue):
-        super().__init__()
-        self.queue = queue
-
-    def emit(self, record):
-        self.queue.put(self.format(record))
-
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger("blueOcean")
 logger.setLevel(logging.DEBUG)
@@ -21,19 +8,22 @@ formatter = logging.Formatter(
     "%(asctime)s [%(levelname)s] | %(processName)s | %(threadName)s | %(module)s | %(message)s"
 )
 
+def _is_notebook() -> bool:
+    return "ipykernel" in sys.modules
+
 if not logger.handlers:
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    if hasattr(sys.modules["__main__"], "__file__"):
-        file_handler = logging.FileHandler(f"logs/{datetime.now()}.log")
+    if not _is_notebook():
+        file_handler = TimedRotatingFileHandler(
+            "logs/blueOcean",
+            when="midnight",
+            backupCount=7,
+            interval=1,
+        )
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-
-    streamlit_handler = StreamlitHandler()
-    streamlit_handler.setLevel(logging.DEBUG)
-    streamlit_handler.setFormatter(formatter)
-    logger.addHandler(streamlit_handler)
